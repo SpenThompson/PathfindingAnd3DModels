ed4ebe7b41d1794162483a50662674b8416a7b7a
Will Harris <william.harris108@gmail.com>
2021-03-21 14:30:59 -0500

    Made a shadow obstacle and made a working agent

--- LightAndDarkness/Assets/Scripts/broMove.cs
+++ LightAndDarkness/Assets/Scripts/broMove.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class broMove : MonoBehaviour
 {
+    public Transform goal;
+    private NavMeshAgent agent;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+        this.agent.SetDestination(goal.position);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = agent.velocity;
+        agent.nextPosition = rb.position;
     }
 
     // Update is called once per frame
