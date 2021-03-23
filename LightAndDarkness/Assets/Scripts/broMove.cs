325d40159d623a58d713c406048afe48bce611ae
Levi <lw03232@gmail.com>
2021-03-23 10:26:30 -0500

    Animated brother

--- LightAndDarkness/Assets/Scripts/broMove.cs
+++ LightAndDarkness/Assets/Scripts/broMove.cs
@@ -6,8 +6,11 @@ using UnityEngine.AI;
 public class broMove : MonoBehaviour
 {
     public Transform goal;
+    public float speed;
     private NavMeshAgent agent;
     private Rigidbody rb;
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +19,21 @@ public class broMove : MonoBehaviour
         agent.updatePosition = false;
         agent.updateRotation = false;
         this.agent.SetDestination(goal.position);
+        animator = gameObject.GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = agent.velocity;
+        speed = rb.velocity.magnitude;
+        if (speed > 0) rb.MoveRotation(Quaternion.LookRotation(rb.velocity));
+
         agent.nextPosition = rb.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("speed", speed);
     }
 }
