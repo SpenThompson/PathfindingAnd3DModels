77bb9f13a26baf867042f4c55c577eba75a60915
Will Harris <william.harris108@gmail.com>
2021-03-23 10:23:33 -0500

    Adjusted LightSwitch script to deal with shadow obstacles

--- LightAndDarkness/Assets/Scripts/SwitchLight.cs
+++ LightAndDarkness/Assets/Scripts/SwitchLight.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SwitchLight : MonoBehaviour
 {
     public GameObject light;
+    public GameObject shadow;
     private Light l;
+    private NavMeshObstacle s;
     // Start is called before the first frame update
     void Start()
     {
         l = light.GetComponent<Light>();
+        s = shadow.GetComponent<NavMeshObstacle>();
     }
 
     // Update is called once per frame
@@ -29,5 +33,6 @@ public class SwitchLight : MonoBehaviour
     public void switchLight()
     {
         l.enabled = !l.enabled;
+        s.enabled = !s.enabled;
     }
 }
