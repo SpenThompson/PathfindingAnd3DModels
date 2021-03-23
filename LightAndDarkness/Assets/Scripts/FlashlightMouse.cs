a9321be09893bd2a9f07439133798e6a7266e4e7
Will Harris <william.harris108@gmail.com>
2021-03-22 20:44:40 -0500

    Trying to get light follow to work

--- LightAndDarkness/Assets/Scripts/FlashlightMouse.cs
+++ LightAndDarkness/Assets/Scripts/FlashlightMouse.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FlashlightMouse : MonoBehaviour
 {
+    private Vector3 pos;
+    private float vertical;
+    private float horizontal;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        pos.x += horizontal;
+        pos.z += vertical;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        vertical = Input.GetAxis("Mouse Y");
+        horizontal = Input.GetAxis("Mouse X");
+        pos = transform.position;
     }
 }
