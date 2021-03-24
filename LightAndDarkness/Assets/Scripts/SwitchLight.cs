1a4e7d9dd3cbdf78c464bdef9b0dc323145f40f7
Will Harris <william.harris108@gmail.com>
2021-03-23 21:54:55 -0500

    Tried fixing buttons

--- LightAndDarkness/Assets/Scripts/SwitchLight.cs
+++ LightAndDarkness/Assets/Scripts/SwitchLight.cs
@@ -23,7 +23,7 @@ public class SwitchLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        keyPressed = Input.GetKeyDown("e");
+        keyPressed = Input.GetKeyDown("E");
     }
 
     private void OnTriggerStay(Collider collision)
