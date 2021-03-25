f9880bc723dd993f0f4c9027da5ed6640605c931
Levi <lw03232@gmail.com>
2021-03-24 23:10:10 -0500

    Hide title text on start

--- LightAndDarkness/Assets/Scripts/GameManager.cs
+++ LightAndDarkness/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
+    public GameObject titleText;
     public GameObject dialogBox;
     public GameObject dialogText;
     private Coroutine dialogCoroutine;
@@ -61,6 +61,7 @@ public class GameManager : MonoBehaviour
     public void StartButton()
     {
         startButton.SetActive(false);
+        titleText.SetActive(false);
         //StartCoroutine(LoadYourAsyncScene("Tutorial"));
         loadScene("Tutorial");
     }
