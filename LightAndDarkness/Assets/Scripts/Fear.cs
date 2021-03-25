c0f06e2f8187987a091a6fa63c14b1a08b1b0eb7
Levi <lw03232@gmail.com>
2021-03-24 19:30:17 -0500

    Fear script updated, still working on hitboxes

--- LightAndDarkness/Assets/Scripts/Fear.cs
+++ LightAndDarkness/Assets/Scripts/Fear.cs
@@ -6,28 +6,38 @@ public class Fear : MonoBehaviour
 {
     public int currentFear= 0;
     public int maxFear = 100;
+    private bool inDark;
 
     public FearGauge fearGauge;
 
-    /*void OnTriggerEnter2D(Collider2D collider2D)
+    void OnTriggerEnter(Collider collider)
     {
-        if (collider2D.CompareTag("Shadow"))
+        if (collider.CompareTag("Shadow"))
         {
-            DamagePlayer(1);
+            if (!inDark) inDark = true;
         }
     }
-    */
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Shadow"))
+        {
+            if (inDark) inDark = false;
+        }
+    }
+    
 
     // Start is called before the first frame update
     void Start()
     {
         currentFear = maxFear;
+        inDark = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (inDark) DamagePlayer(1);
     }
 
     public void DamagePlayer(int damage)
